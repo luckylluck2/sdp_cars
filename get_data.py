88c23d2ce06996f2592f617728eade08cffc7d71
Luc van Schijndel <lcvsch@hotmail.com>
2023-02-22 11:08:15 +0100

    load data dign

--- get_data.py
+++ get_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
@@ -11,7 +12,15 @@ api = KaggleApi()
 # By default, the authenticate function looks for a '~/.kaggle/kaggle.json' file
 # to connect to your Kaggle account. 
 api.authenticate()
-api.dataset_download_files('irkaal/foodcom-recipes-and-reviews', 
+api.dataset_download_files('austinreese/craigslist-carstrucks-data', 
                            path=data_dir, 
                            quiet=False, # show download progress
                            unzip=True) # unzip the downloaded archive
+
+data_folder = './data'
+vehicles_file = 'vehicles.csv'
+vehicles_parquet = 'vehicles.parquet'
+
+data = pd.read_csv(os.path.join(data_folder, vehicles_file))
+
+data.to_parquet(os.path.join(data_folder, vehicles_parquet))
