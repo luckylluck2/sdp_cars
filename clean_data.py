f5735da1a6360dd4c72847b1a12bef6745a82c02
Luc van Schijndel <lcvsch@hotmail.com>
2023-02-22 14:08:42 +0100

    cleaned data

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn.preprocessing import OneHotEncoder
 
 # Load vehicles data
 
@@ -8,20 +9,21 @@ clean_data_folder = './data'
 vehicles_file = 'vehicles.parquet'
 cleaned_file = 'cleaned_data.parquet'
 
-og_data = pd.read_parquet(os.path.join(clean_data_folder, vehicles_file))
-
-og_data.columns
-
-
-# Select appropriate columns
+odometer_cutoffPoint = 400000   #in miles
+price_cutoffPoint = 500000      #in USD
 
 required_columns = ['price', 'year', 'manufacturer', 'condition', 'cylinders', 'fuel', 'odometer',
                     'title_status', 'transmission', 'drive', 'size', 'type', 'paint_color']
 
-clean_data = og_data[required_columns]
-# Remove rows with missing values
+og_data = pd.read_parquet(os.path.join(clean_data_folder, vehicles_file))
 
-og_data['cylinders']
+#drop rows with extremely large prices
+og_data.drop(np.where(og_data['price'] > price_cutoffPoint)[0], inplace=True)
+
+# Select appropriate columns
+
+
+
 
 def cylinders_to_numeric(cylinderstring):
     """
@@ -31,80 +33,38 @@ def cylinders_to_numeric(cylinderstring):
     else: return(int(cylinderstring.replace(' cylinders', '')))
 
 og_data['cylinders'] = og_data['cylinders'].map(cylinders_to_numeric)
+#also, impute any missing values with the mean
 og_data['cylinders'].fillna(og_data['cylinders'].mean(skipna=True))
 
+#we also add some additional features:
+og_data['yearSquared'] = og_data['year']**2     #to explain behaviour with extremele new/old cars
 
-def timestring_to_minutes(timestring):
-    """
-    Convert the timestring in the columns 'CookTime', 'PrepTime' or 'TotalTime' to minutes.
-    Example: 'PT24H45M' -> 24 * 60 + 45
-    
-    Args:
-        timestring (str): String indicating a time duration
-        
-    Returns:
-        total_time (float): Number of minutes
-    """
-    if type(timestring) is str:
-        # Remove leading 'PT'
-        timestring = timestring.replace('PT', '')
-        
-        # Determine the number of hours from the remaining string before 'H'
-        if 'H' in timestring:
-            timestring_h = timestring.split('H')
-            hours = int(timestring_h[0])
-            
-            # Consider only the part after 'H' for the minutes
-            timestring = timestring_h[1]
-        else:
-            hours = 0
-            
-        # Determine the number of minutes from the remaining string before 'M'
-        if 'M' in timestring:
-            minutes = int(timestring.split('M')[0])
-        else:
-            minutes = 0
-        
-        # Calculate the total time duration in minutes
-        total_time = hours * 60 + minutes
-        return total_time
-    else:
-        return None
-
-#Apply function to all time-valued columns
-clean_data['CookTime'] = clean_data['CookTime'].map(timestring_to_minutes)
-clean_data['PrepTime'] = clean_data['PrepTime'].map(timestring_to_minutes)
-clean_data['TotalTime'] = clean_data['TotalTime'].map(timestring_to_minutes)
-clean_data = clean_data.astype({'RecipeId': 'int32', 'CookTime': 'float64',
-                    'PrepTime': 'float64', 'TotalTime': 'float64'})
 
 #Next, we ignore rows with unrealistic values. Any abnormally high values for specific columns are removed.
-temp_filter = (clean_data['Calories'] <= 10000) & (clean_data['PrepTime'] <= 120) & (clean_data['CookTime'] <= 180) & \
-                (clean_data['SodiumContent'] <= 5000)
-clean_data = clean_data[temp_filter]
-
-#remove the TotalTime column, since it is not always sum of prep and cook times. 
-clean_data = clean_data.drop(columns='TotalTime')
+#The odometer has some unrealistic values, so any value exceeding a threshold (set above) is set to be missing
+og_data['odometer'][og_data['odometer'] > odometer_cutoffPoint] = np.nan
+#and we impute missing values with the mean
+og_data['odometer'].fillna(og_data['odometer'].mean(skipna=True))
 
 
-#Next up, we convert the *Content columns to its respective percentage daily value amount
-daily_values = {'Calories': 200,            #gram? (Seems to be calories from fat)
-                'FatContent': 65,           #gram
-                'SaturatedFatContent': 20,  #gram
-                'CholesterolContent': 300,  #milligram
-                'SodiumContent': 2400,      #milligram
-                'CarbohydrateContent': 300, #gram
-                'FiberContent': 25,         #gram
-                'SugarContent': 25,         #gram
-                'ProteinContent': 50}       #gram 
+#select only the wanted columns
+clean_data = og_data[required_columns]
 
-for columnName in daily_values.keys():
-    #Calculate percentage daily value (as decimal value; NOT percentage)
-    clean_data[columnName] = clean_data[columnName]/daily_values[columnName]
-    #and rename column to include PDV
-    newName = columnName.replace('Content', '')
-    newName = newName + 'PDV'
-    clean_data = clean_data.rename(columns={columnName:newName})
+#ONE HOT ENCODING! WHOOP WHOOP!
+categorical_columns = ['manufacturer', 'condition', 'fuel', 'title_status', 'transmission',
+                       'drive', 'size', 'type', 'paint_color'] 
+enc = OneHotEncoder(sparse=False).fit(clean_data[categorical_columns])
+encoded = enc.transform(clean_data[categorical_columns])
+encoded_df = pd.DataFrame(encoded, columns=enc.get_feature_names_out()) 
+# encoded_df.columns.to_list()
 
+non_categorical_columns = ['price', 'year', 'odometer', 'cylinders']
+
+
+clean_data = pd.concat([clean_data[non_categorical_columns].copy(), encoded_df], axis = 1)
+
+#clean_data.dtypes
 
 clean_data.to_parquet(os.path.join(clean_data_folder, cleaned_file))
+
+len(clean_data)
