97ec030c1f63fca5d5f8c78c707bbe6e1a9ac396
Luc van Schijndel <lcvsch@hotmail.com>
2023-02-27 11:09:42 +0100

    Tried some dimension reduction

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -25,9 +25,9 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
                                                     test_size=0.10/0.90, random_state=42)
 
 scaler = StandardScaler().fit(X_train)
-X_train_transformed = scaler.transform(X_train)
-X_test_transformed  = scaler.transform(X_test)
-X_val_transformed  = scaler.transform(X_val)
+X_train_transformed = pd.DataFrame(scaler.transform(X_train), columns= X_train.columns)
+X_test_transformed  = pd.DataFrame(scaler.transform(X_test), columns= X_test.columns)
+X_val_transformed  = pd.DataFrame(scaler.transform(X_val) ,columns= X_val.columns)
 
 pca = PCA(n_components = 2).fit(X_train_transformed)
 
@@ -40,3 +40,15 @@ interesting_columns = [np.where(np.abs(pca.components_[0]) > component_threshold
 print(''.join([f'ColName: {cars_data.columns[i]}; \tWeight: {round(pca.components_[0][i], 2)}\n' \
                for i in interesting_columns[0][0]]))
 #beetje useless dit vind ik
+# Misschien als we alleen op jaar en odometer PCA doen:
+
+pca_limited = PCA(n_components = 2).fit(X_train_transformed['odometer', 'year'])
+
+print(pca_limited.explained_variance_ratio_)
+print(pca_limited.singular_values_)
+component_threshold_value = [np.flip(np.sort(np.abs(pca_limited.components_[0])))[9], np.flip(np.sort(np.abs(pca_limited.components_[1])))[9]]
+interesting_columns = [np.where(np.abs(pca_limited.components_[0]) > component_threshold_value[0]),
+                       np.where(np.abs(pca_limited.components_[1]) > component_threshold_value[1])]
+
+print(''.join([f'ColName: {cars_data.columns[i]}; \tWeight: {round(pca_limited.components_[0][i], 2)}\n' \
+               for i in interesting_columns[0][0]]))
