2332356d7120ca9faa2f0cec391351333b7b494f
Luc van Schijndel <lcvsch@hotmail.com>
2023-03-01 09:08:53 +0100

    Interpretations added

--- regression_analysis.py
+++ regression_analysis.py
@@ -5,67 +5,74 @@ from scipy.stats import norm
 import os
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-
-
 
 # Load recipes data
 
 data_folder = './data'
-recipes_file = 'cleaned_data.parquet'
+cars_data_train = 'cleaned_data_train.parquet'
+cars_data_test = 'cleaned_data_test.parquet'
+cars_data_val = 'cleaned_data_val.parquet'
+cars_price_train = 'cleaned_price_train.parquet'
+cars_price_test = 'cleaned_price_test.parquet'
+cars_price_val = 'cleaned_price_val.parquet'
 
-cars_data = pd.read_parquet(os.path.join(data_folder, recipes_file))
 cars_data_cat = pd.read_parquet(os.path.join(data_folder, 'cleaned_data_categorical.parquet'))
 
+y_train = pd.read_parquet(os.path.join(data_folder, cars_price_train))
+y_train.index = range(len(y_train))
+y_test = pd.read_parquet(os.path.join(data_folder, cars_price_test))
+y_test.index = range(len(y_test))
+y_val = pd.read_parquet(os.path.join(data_folder, cars_price_val))
+y_val.index = range(len(y_val))
+X_train = pd.read_parquet(os.path.join(data_folder, cars_data_train))
+X_train.index = range(len(y_train))
+X_test = pd.read_parquet(os.path.join(data_folder, cars_data_test))
+X_test.index = range(len(y_test))
+X_val = pd.read_parquet(os.path.join(data_folder, cars_data_val))
+X_val.index = range(len(y_val))
 
-cars_prices = cars_data['price']
-cars_data.drop(columns='price', inplace=True)
+indicator_column_names = X_train.columns[5:]
 
-indicator_column_names = cars_data.columns[3:]
+price_scaler = MinMaxScaler().fit(np.array(y_train).reshape(-1,1))
+y_train = pd.DataFrame(price_scaler.transform(np.array(y_train).reshape(-1,1)), columns= ['price'])
+y_test = pd.DataFrame(price_scaler.transform(np.array(y_test).reshape(-1,1)), columns= ['price'])
+y_val = pd.DataFrame(price_scaler.transform(np.array(y_val).reshape(-1,1)), columns= ['price'])
 
-X_train, X_test, y_train, y_test = train_test_split(cars_data, cars_prices, 
-                                                    test_size=0.10, random_state=37)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, 
-                                                    test_size=0.10/0.90, random_state=42)
+cov_scaler = StandardScaler().fit(X_train)
+X_train_t = pd.DataFrame(cov_scaler.transform(X_train), columns= X_train.columns)
+X_train_t.index = X_train.index
+X_train_t[indicator_column_names] = X_train[indicator_column_names]
+X_test_t = pd.DataFrame(cov_scaler.transform(X_test), columns= X_test.columns)
+X_test_t.index = X_test.index
+X_test_t[indicator_column_names] = X_test[indicator_column_names]
+X_val_t = pd.DataFrame(cov_scaler.transform(X_val), columns= X_val.columns)
+X_val_t.index = X_val.index
+X_val_t[indicator_column_names] = X_val[indicator_column_names]
 
-price_scaler = StandardScaler().fit(np.array(y_train).reshape(-1,1))
-y_train_transformed = price_scaler.transform(np.array(y_train).reshape(-1,1))
-y_test_transformed = price_scaler.transform(np.array(y_test).reshape(-1,1))
-y_val_transformed = price_scaler.transform(np.array(y_val).reshape(-1,1))
 
-scaler = StandardScaler().fit(X_train)
-X_train_transformed = pd.DataFrame(scaler.transform(X_train), columns= X_train.columns)
-X_train_transformed.index = X_train.index
-X_train_transformed[indicator_column_names] = X_train[indicator_column_names]
-X_test_transformed  = pd.DataFrame(scaler.transform(X_test), columns= X_test.columns)
-X_test_transformed.index = X_test.index
-X_test_transformed[indicator_column_names] = X_test[indicator_column_names]
-X_val_transformed  = pd.DataFrame(scaler.transform(X_val), columns= X_val.columns)
-X_val_transformed.index = X_val.index
-X_val_transformed[indicator_column_names] = X_val[indicator_column_names]
+X_train, X_test, X_val = X_train_t, X_test_t, X_val_t
 
-lin_mod = LinearRegression().fit(X_train_transformed, y_train_transformed)
+lin_mod = LinearRegression().fit(X_train, y_train)
 
-y_pred_lm = lin_mod.predict(X_test_transformed)
+y_pred_lm = lin_mod.predict(X_test)
 
-print(lin_mod.coef_)
+rmse_lm = np.sqrt(mean_squared_error(y_pred_lm, y_test))
 
-rmse_lm = np.sqrt(mean_squared_error(y_pred_lm, y_test_transformed))
-print(f'rmse: {rmse_lm}')
 np.array(np.round(y_pred_lm[:10]))
-np.array(y_test_transformed.iloc[:10])
+np.array(y_test.iloc[:10])
 
 c = [i for i in range(len(y_pred_lm))]
 fig = plt.figure()
-plt.plot(c,y_test_transformed-y_pred_lm, color="blue", linewidth=0.5, linestyle="-")
+plt.plot(c,y_test-y_pred_lm, color="blue", linewidth=0.5, linestyle="-")
 fig.suptitle('Error Terms', fontsize=20)              # Plot heading 
 plt.xlabel('Index', fontsize=18)                      # X-label
 plt.ylabel('ytest-ypred', fontsize=16)                # Y-label
 plt.show()
 
-errors = y_test_transformed - y_pred_lm
+errors = y_test - y_pred_lm
 
 (mu, sigma) = norm.fit(errors)
 
@@ -74,56 +81,62 @@ y = norm.pdf( bins, mu, sigma)
 l = plt.plot(bins, y, 'r--', linewidth=1)
 plt.show()
 
-print(f'rmse: {rmse_lm}')
-
 
 #what about SVM/SVR?
 
 from sklearn.svm import SVR
 
 #C a bit decreased, since we do have some outliers/noisy data
-svr = SVR(C = 0.8, epsilon=0.1, kernel= 'linear').fit(X_train_transformed[:10000], 
-                                                      np.ravel(y_train_transformed[:10000]))
+svr = SVR(C = 0.8, epsilon=0.1, kernel= 'linear').fit(X_train[:10000], 
+                                                      np.ravel(y_train[:10000]))
 print(svr.coef_.round(2))
 # print('Support vectors: \n' + ''.join(str(svr.support_vectors_)))
-y_pred_svr = svr.predict(X_test_transformed)
-rmse_svr = np.sqrt(mean_squared_error(y_pred_svr, y_test_transformed))
+y_pred_svr = svr.predict(X_test)
+rmse_svr = np.sqrt(mean_squared_error(y_pred_svr, y_test))
 
 from sklearn.neighbors import KNeighborsRegressor, RadiusNeighborsRegressor
 # het gemiddelde van neighbors is de voorspelling
 
-knr = KNeighborsRegressor(n_neighbors=11).fit(X_train_transformed, y_train_transformed)
-y_pred_knr = knr.predict(X_test_transformed)
-rmse_knr = np.sqrt(mean_squared_error(y_pred_knr, y_test_transformed))
+knr = KNeighborsRegressor(n_neighbors=11).fit(X_train, y_train)
+y_pred_knr = knr.predict(X_test)
+rmse_knr = np.sqrt(mean_squared_error(y_pred_knr, y_test))
 
 #rnr ruineert memory, dus nee
 
 from sklearn.tree import DecisionTreeRegressor
 # maak een boom, en fit daarmee dan een step-function op de data.
 
-dtr = DecisionTreeRegressor(random_state=0).fit(X_train_transformed, y_train_transformed)
-y_pred_dtr = dtr.predict(X_test_transformed)
-rmse_dtr = np.sqrt(mean_squared_error(y_pred_dtr, y_test_transformed))
+dtr = DecisionTreeRegressor(random_state=0).fit(X_train, y_train)
+y_pred_dtr = dtr.predict(X_test)
+rmse_dtr = np.sqrt(mean_squared_error(y_pred_dtr, y_test))
+
+import sklearn.inspection as ski
+
+perm_imp = ski.permutation_importance(dtr, X_val, y_val)
+
+plt.scatter(x = range(len(perm_imp['importances_mean'])), y = perm_imp['importances_mean'])
+plt.show()
 
 #requires y_test and y_pred as known variables
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.jointplot(x = y_test_transformed.flatten(), y = y_pred_dtr.flatten(), kind = 'hist')
-# plt.plot(y_test_transformed.flatten(), y_test_transformed.flatten(), 'r--')
+sns.jointplot(x = y_test.T.to_numpy()[0], y = y_pred_dtr, 
+              kind = 'scatter', alpha = 0.2, s = 2)
+# plt.plot(y_test.flatten(), y_test.flatten(), 'r--')
 plt.xlabel('Real prices')
 plt.ylabel('Predicted prices')
 plt.show()
 
 # Lasso en Ridge regression
 from sklearn.linear_model import Lasso, Ridge
-lasso = Lasso(alpha = 0.4).fit(X_train_transformed, y_train_transformed)
-y_pred_las = lasso.predict(X_test_transformed)
-rmse_las = np.sqrt(mean_squared_error(y_pred_las, y_test_transformed))
+lasso = Lasso(alpha = 0.4).fit(X_train, y_train)
+y_pred_las = lasso.predict(X_test)
+rmse_las = np.sqrt(mean_squared_error(y_pred_las, y_test))
 
-ridge = Ridge(alpha = 1.0).fit(X_train_transformed, y_train_transformed)
-y_pred_rid = ridge.predict(X_test_transformed)
-rmse_rid = np.sqrt(mean_squared_error(y_pred_rid, y_test_transformed))
+ridge = Ridge(alpha = 1.0).fit(X_train, y_train)
+y_pred_rid = ridge.predict(X_test)
+rmse_rid = np.sqrt(mean_squared_error(y_pred_rid, y_test))
 
 
 print(f'Linear model RMSE: {rmse_lm}\n' + 
@@ -133,21 +146,10 @@ print(f'Linear model RMSE: {rmse_lm}\n' +
       f'Lasso model RMSE: {rmse_las}\n' + 
       f'Ridge model RMSE: {rmse_rid}\n')
 
-ridge.coef_.round(2)
-X_train.columns
 
-output_string = [(f'{(ridge.coef_[0][i]*price_scaler.scale_)[0]: .0f}'+ ' ' + X_train.columns[i]) for i in range(len(X_train.columns))]
+output_string = [(f'{(ridge.coef_[0][i]*price_scaler.scale_)[0]: .2f}'+ ' ' + X_train.columns[i]) for i in range(len(X_train.columns))]
 print('\n'.join(output_string))
-price_scaler.scale_
-price_scaler.mean_
-price_scaler.inverse_transform(np.array([0,1]).reshape(-1,1))
 
-scaler.mean_[2]
-scaler.scale_[2]
-ridge.coef_[0]
-foo = scaler.inverse_transform(np.array([int(i == 0) for i in range(210)]).reshape(2, 105))
-foo[0] - foo[1]
-foo[1] - scaler.mean_
 
 def interpret_numeric_coefs(coefs, names, price_scalar, feature_scalar):
       numeric_scales   = (lambda numeric_coef : (np.array(numeric_coef)/feature_scalar.scale_[:3])*
@@ -192,11 +194,28 @@ def plot_interpretation(interpretation):
       plt.xticks(rotation=90)
       plt.show()
 
-print_interpretation(interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, scaler, 'manufacturer'))
-plot_interpretation(interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, scaler, 'manufacturer'))
+# print_interpretation(interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, cov_scaler, 'manufacturer'))
+# plot_interpretation(interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, cov_scaler, 'manufacturer'))
+# print_interpretation(['Decision tree', X_train.columns, perm_imp['importances_mean']*100])
 
-for category in ['manufacturer', 'condition', 'fuel', 
-                                   'title_status', 'transmission', 'drive', 
-                                   'size', 'type', 'paint_color']:
-      print_interpretation(interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, scaler, category))
-      plot_interpretation(interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, scaler, category))
+
+
+categories = ['manufacturer', 'condition', 'fuel', 
+              'title_status', 'transmission', 'drive', 
+              'size', 'type', 'paint_color',
+              'numeric']
+
+#sums to total of 259556.6 USD
+#foo sums to total of 3.1
+
+for category in categories:
+      print(category)
+      cat_interpretation = interpret_numeric_coefs(ridge.coef_[0], X_train.columns, price_scaler, cov_scaler) if category == 'numeric' else interpret_coefs(ridge.coef_[0], X_train.columns, price_scaler, cov_scaler, category)
+      # print(cat_interpretation)
+      print(f'Total weight category (Ridge): {np.sum(np.abs(cat_interpretation[2]))/259556.6*100}')
+      foo = np.sum(perm_imp['importances_mean'] * [X_train.columns[i].__contains__(category) for i in range(len(X_train.columns))])
+      print(f'Total weight category (DTR): {foo/3.1*100: .2f}')
+      #foo for numerics: {'year': 0.815, 'odometer': 0.524, 'cylinders': 0.35}
+      # print_interpretation(cat_interpretation)
+      plot_interpretation(cat_interpretation)
+
