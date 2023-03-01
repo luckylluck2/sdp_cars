e1e15873947055d74aa5fc2ccc4acf7b80e1fcfa
Matthijs van Groeningen <matthijsvangroeningen@gmail.com>
2023-03-01 09:52:39 +0100

    dnn training update

--- machine_learning/DNN/model.py
+++ machine_learning/DNN/model.py
@@ -2,21 +2,21 @@ import torch.nn as nn
 
 class DNN(nn.Module):
 
-    def __init__(self, hidden_layers=3, hidden_dim=32, input_dim=3, output_dim=2):
+    def __init__(self, hidden_dims, input_dim=3, output_dim=2):
         super(DNN, self).__init__()
-        self.hidden_layers = hidden_layers
-        self.hidden_dim = hidden_dim
+        self.hidden_layers = len(hidden_dims)
+        self.hidden_dims = hidden_dims
         self.input_dim = input_dim
         self.output_dim = output_dim
         
-        self.input_layer = nn.Linear(self.input_dim, self.hidden_dim)
-        self.output_layer = nn.Linear(self.hidden_dim, self.output_dim)
+        self.input_layer = nn.Linear(self.input_dim, self.hidden_dims[0])
+        self.output_layer = nn.Linear(self.hidden_dims[-1], self.output_dim)
         
         model_layers = []
         model_layers.append(self.input_layer)
         model_layers.append(nn.ReLU())
-        for i in range(self.hidden_layers):
-            model_layers.append(nn.Linear(self.hidden_dim, self.hidden_dim))
+        for i in range(self.hidden_layers - 1):
+            model_layers.append(nn.Linear(self.hidden_dims[i], self.hidden_dims[i + 1]))
             model_layers.append(nn.ReLU())
         model_layers.append(nn.Dropout(p=0.5))
         model_layers.append(self.output_layer)
--- machine_learning/DNN/training.py
+++ machine_learning/DNN/training.py
@@ -1,55 +1,17 @@
 import os
-from torch.nn import CrossEntropyLoss
+import torch
+from torch.nn import MSELoss
 import numpy as np
 import torch.optim as optim
 from time import sleep
 from tqdm import tqdm
 from torch import save
-from torch.nn.functional import softmax
-
-
-def calculate_metrics(tp, tn, fp, fn):
-    """Calculates a number of metrics based on the number of true positives,
-    true negatives, false positives and false negatives.
-    Args:
-        tp (int): Number of true positives
-        tn (int): Number of true negatives
-        fp (int): Number of false positives
-        fn (int): Number of false negatives
-    Returns:
-        precision (float): Fraction of positive classifications that were correct
-        recall (float): Fraction of positive examples that were correctly classified (true positive rate)
-        selectivity (float): Fraction of negative examples that were correctly classified (true negative rate)
-        accuracy (float): Fraction of examples that were correctly classified
-        balanced_accuracy (float): Mean of the recall and selectivity
-        f1 (float): F1 score, harmonic mean of the precision and recall
-    """
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    selectivity = tn / (tn + fp)
-    accuracy = (tp + tn) / (tp + tn + fp + fn)
-    balanced_accuracy = (recall + selectivity) / 2
-    f1 = 2 * tp / (2 * tp + fp + fn)
-
-    metric_dict = {'precision': precision,
-                   'recall': recall,
-                   'selectivity': selectivity,
-                   'accuracy': accuracy,
-                   'balanced_accuracy': balanced_accuracy,
-                   'f1': f1}
-
-    return metric_dict
 
 
 def step(data_set, model, optimizer, criterion, epoch, mode='train'):
-    losses = []
+    rmses = []
     data_size = len(data_set)
     progress_bar = tqdm(np.arange(data_size), desc=f'{mode} epoch {epoch}', total=data_size)
-
-    true_positives = 0
-    true_negatives = 0
-    false_positives = 0
-    false_negatives = 0
     
     if mode == 'train':
         model.train()
@@ -59,24 +21,7 @@ def step(data_set, model, optimizer, criterion, epoch, mode='train'):
     for i, (x, y) in enumerate(data_set):
         out = model(x)
         loss = criterion(out, y)
-        losses.append(loss.item())
-        
-        # predictions = softmax(out, 1).detach().numpy()[:, 1] > 0.5
-        # truths = y.detach().numpy()[:, 1] > 0.5
-        # output_pairs = np.stack((predictions, truths), axis=-1)
-        
-        # true_positives += np.sum(np.all(output_pairs == np.array([True, True]), axis=1))
-        # true_negatives += np.sum(np.all(output_pairs == np.array([False, False]), axis=1))
-        # false_positives += np.sum(np.all(output_pairs == np.array([True, False]), axis=1))
-        # false_negatives += np.sum(np.all(output_pairs == np.array([False, True]), axis=1))
-        # predictions = softmax(out, 1).detach().numpy()[:, 1] > 0.5
-        # truths = y.detach().numpy()[:, 1] > 0.5
-        # output_pairs = np.stack((predictions, truths), axis=-1)
-
-        # true_positives += np.sum(np.all(output_pairs == np.array([True, True]), axis=1))
-        # true_negatives += np.sum(np.all(output_pairs == np.array([False, False]), axis=1))
-        # false_positives += np.sum(np.all(output_pairs == np.array([True, False]), axis=1))
-        # false_negatives += np.sum(np.all(output_pairs == np.array([False, True]), axis=1))
+        rmses.append(np.sqrt(loss.item()))
 
         if mode == 'train':
             optimizer.zero_grad()
@@ -88,24 +33,16 @@ def step(data_set, model, optimizer, criterion, epoch, mode='train'):
         if i < len(data_set) - 1:
             progress_bar.update()
 
-    loss = np.mean(losses)
-    # metrics = calculate_metrics(true_positives, true_negatives, false_positives, false_negatives)
-
-    metrics = {'precision': 0,
-                   'recall': 0,
-                   'selectivity': 0,
-                   'accuracy': 0,
-                   'balanced_accuracy': 0,
-                   'f1': 0}
+    rmse = np.mean(rmses)
     
-    progress_bar.set_postfix({'loss': loss, 
-                              'tp': true_positives,
-                              'tn': true_negatives,
-                              'fp': false_positives,
-                              'fn': false_negatives,})
+    progress_bar.set_postfix({'rmse': rmse})
     progress_bar.update()
 
-    return loss, metrics
+    return rmse
+
+
+def MAPELoss(output, target):
+    return torch.mean(torch.abs((target - output) / target)) 
 
 
 def train_model(model, model_save_dir, train_dataset, val_dataset=None, num_epochs=40, lr=1e-4, 
@@ -114,32 +51,18 @@ def train_model(model, model_save_dir, train_dataset, val_dataset=None, num_epoc
     if not os.path.exists(model_save_dir):
         os.mkdir(model_save_dir)
 
-    criterion = CrossEntropyLoss(reduction='sum')
+    criterion = MSELoss()
+    # criterion = MAPELoss()   
+    # criterion = CrossEntropyLoss(reduction='sum')
     optimizer = optim.Adam([{'params': model.parameters()}], lr=lr, weight_decay=l2)
 
     min_loss = np.inf
     epochs_without_improvement = 0
-    
-    metrics_dict = {'train_loss': [], 'val_loss': [],
-                    'train_precision': [], 'val_precision': [],
-                    'train_recall': [], 'val_recall': [],
-                    'train_selectivity': [], 'val_selectivity': [],
-                    'train_accuracy': [], 'val_accuracy': [],
-                    'train_balanced_accuracy': [], 'val_balanced_accuracy': [],
-                    'train_f1': [], 'val_f1': []}
 
     for epoch in range(num_epochs):
         for mode, dataset in zip(['train', 'val'], [train_dataset, val_dataset]):
             if dataset is not None:
-                loss, metrics = step(dataset, model, optimizer, criterion, epoch, mode)
-
-                metrics_dict[f'{mode}_loss'].append(loss)
-                metrics_dict[f'{mode}_precision'].append(metrics['precision'])
-                metrics_dict[f'{mode}_recall'].append(metrics['recall'])
-                metrics_dict[f'{mode}_selectivity'].append(metrics['selectivity'])
-                metrics_dict[f'{mode}_accuracy'].append(metrics['accuracy'])
-                metrics_dict[f'{mode}_balanced_accuracy'].append(metrics['balanced_accuracy'])
-                metrics_dict[f'{mode}_f1'].append(metrics['f1'])
+                loss = step(dataset, model, optimizer, criterion, epoch, mode)
         
         # Prevent printing of progress bars from messing up
         print(' ')
@@ -158,5 +81,3 @@ def train_model(model, model_save_dir, train_dataset, val_dataset=None, num_epoc
         if epochs_without_improvement >= early_stopping_threshold:
             print(f'Early stopping after {epoch} epochs')
             break
-    
-    return metrics_dict
